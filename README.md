77617011f1fc28271ad1736a18552e7f2ec4f620
HARSHIT SINGH <realharshitsingh@gmail.com>
2021-01-12 18:07:41 +0530

    changes 1. Add README.md 2. Move sound files in Sound Folder 3. Move gif files in gif folder 4. Remove blank lines

---
+++ README.md
@@ -0,0 +1,8 @@
+# Snake game Project
+## snake game using turtle and Pygame module in python 
+
+##controls
+**W** - *Move Forward*
+**S** - *Move Backward*
+**D** - *Move Right*
+**A** - *Move Left*
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Import MOdule
-
+# Import Mddule
 import turtle
 import time
 import random
@@ -8,19 +7,16 @@ delay =0.1
 score=0
 high_score=0
 pygame.mixer.init()
-pygame.mixer.music.load("gta_san_andreas.mp3")
+pygame.mixer.music.load("./sound/gta_san_andreas.mp3")
 pygame.mixer.music.play(1000)
-
 # background image
 # bg = pygame.image.load("snake2.jpg")
-
 # Screen 
 win = turtle.Screen()
 win.setup(width=900,height=550)
 win.title(" \t!!!SNAKE GAME!!!\t")
-win.bgpic('snake.gif')
+win.bgpic('./gif/snake.gif')
 win.tracer(0)
- 
 # pen 
 pen =turtle.Turtle()
 pen.speed(0)
@@ -29,11 +25,6 @@ pen.penup()
 pen.goto(-360,150)
 pen.hideturtle()
 pen.write("SCORE =0 \nHIGHSCORE =0", move=False, align="left",font=('Arial', 20, 'bold'))
-
-
-
-
-
 # Snake Head
 head = turtle.Turtle()
 head.shape("circle")
@@ -43,22 +34,15 @@ head.speed(0)
 head.penup()
 head.goto(10,10)
 head.direction="stop"
-
-
 # food 
-
 food = turtle.Turtle()
 food.shape("turtle")
 food.fillcolor("#0000ff")
 food.pencolor("red")
-
 food.speed(0)
 food.penup()
 food.goto(0,100)
-
-
 segments =[]
-
 
 # For  control
 class Direct:
@@ -75,7 +59,6 @@ class Direct:
         if head.direction != "left":
             head.direction = "right"
     
-
 # For move
 class Move():
     def move(self):
@@ -101,61 +84,47 @@ win.onkeypress(dr.go_down,"s")
 win.onkeypress(dr.go_left,"a")               
 
 # Main game loop
-
 while True:
     win.update()
-    # score
-    
+    # score 
     # for chechking for barrier
-    if head.xcor()> 450 or head.xcor()<-450 or head.ycor() > 260 or head.ycor() <-260:
-        
-        pygame.mixer.music.load("game_over_sms.mp3")
+    if head.xcor()> 450 or head.xcor()<-450 or head.ycor() > 260 or head.ycor() <-260:       
+        pygame.mixer.music.load("./sound/game_over_sms.mp3")
         pygame.mixer.music.play()
         time.sleep(1.5)
-        pygame.mixer.music.load("gta_san_andreas.mp3")
+        pygame.mixer.music.load("./sound/gta_san_andreas.mp3")
         pygame.mixer.music.play(1000)
-      
         head.goto(0,0)
         head.direction ="stop"
-    # hide the segment 
+        # hide the segment 
         for segment in segments :
-            segment.goto(1000,1000)
-
-        
-    # clear the segment
-        segments.clear()
-    # Reset score
-        score=0
+            segment.goto(1000,1000)       
+        segments.clear()     # clear the segment
+        score=0    # Reset score
     pen.clear()
     pen.write(f"SCORE = {score}\nHIGHSCORE={high_score}".format(score,high_score),move=False, align="left",font=('Arial', 20, 'bold'))
     # Collide snake to his segment
     for segment in segments :
         if segment.distance(head) < 20:
-            pygame.mixer.music.load("game_over_sms.mp3")
+            pygame.mixer.music.load("./sound/game_over_sms.mp3")
             pygame.mixer.music.play()
             time.sleep(1.5)
-            pygame.mixer.music.load("gta_san_andreas.mp3")
-            pygame.mixer.music.play(1000)
-            
+            pygame.mixer.music.load("./sound/gta_san_andreas.mp3")
+            pygame.mixer.music.play(1000)            
             head.goto(0,0)
             head.direction="stop"
-            
-     # hide the segment 
+            # hide the segment 
             for segment in segments :
-                segment.goto(1000,1000)
-
-        
-    # clear the segment
-            segments.clear()
-    # Reset score
-            score=0
+                segment.goto(1000,1000)      
+            segments.clear() # clear the segment
+            score=0 # Reset score
         pen.clear()
         pen.write(f"SCORE = {score}\nHIGHSCORE={high_score}".format(score,high_score),move=False, align="left",font=('Arial', 20, 'bold'))
     if head.distance(food) < 20:
         x= random.randint(-430, 430)
         y= random.randint(-250, 250)
         food.goto(x,y)
-# Add segments 
+        # Add segments 
         new_segments = turtle.Turtle()
         new_segments.speed(0)
         new_segments.shape("circle")
@@ -168,24 +137,16 @@ while True:
             high_score = score
         pen.clear()
         pen.write(f"SCORE = {score}\nHIGHSCORE={high_score}".format(score,high_score),move=False, align="left",font=('Arial', 20, 'bold'))
-        with open("highscore","w") as f:
+        with open("highscore.txt","w") as f:
             f.write(str(high_score))
     for index in range(len(segments)-1,0,-1):
         x= segments[(index)-1].xcor()
         y= segments[(index)-1].ycor()
         segments[index].goto(x,y)
-        
-    
-    
     if len(segments) > 0 :
         x= head.xcor()
         y=head.ycor()
         segments[0].goto(x,y)
-
-
     mv.move()
-
     time.sleep(delay)
-
-
 win.mainloop()
